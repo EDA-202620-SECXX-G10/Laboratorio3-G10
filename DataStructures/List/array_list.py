4e148e53284f960c1a6685345f7524e1548a04df
Matthewhack <m.pulidom2@uniandes.edu.co>
2026-08-26 22:47:32 -0500

    arreglos listas

--- DataStructures/List/array_list.py
+++ DataStructures/List/array_list.py
@@ -1,3 +1,4 @@
+import random
 def new_list():
     newlist ={
         'elements' : [],
@@ -30,9 +31,9 @@ def size(my_list):
     return my_list["size"]
 
 def last_element(my_list):
-    return my_list["elements"]["size"]-1
+    return my_list["elements"][my_list["size"]-1]
 
-def delete_element(my_list, pos)
+def delete_element(my_list, pos):
     del my_list["elements"][pos]
     my_list["size"] -= 1
     return my_list
@@ -56,10 +57,15 @@ def change_info(my_list, new_info, pos):
     my_list["elements"][pos] = new_info
     return my_list
 
-def exchange(my_list, pos1, pos2):
+def exchange(my_list, pos_1, pos_2):
     temporal = my_list["elements"][pos_1]
     my_list["elements"][pos_1] = my_list["elements"][pos_2]
     my_list["elements"][pos_2] = temporal
+    return my_list
+
+def add_last(my_list, element):
+    my_list["elements"].append(element)
+    my_list["size"] += 1
     return my_list
 
 def sub_list(my_list, pos_i, num_elements):
@@ -67,3 +73,130 @@ def sub_list(my_list, pos_i, num_elements):
     for i in range(pos_i, pos_i + num_elements):
         insert_element(newlist, my_list["elements"][i], size(newlist))
     return newlist
+
+
+def default_sort_criteria(element1, element2):
+    is_sorted = False
+    if element1 <= element2:
+        is_sorted = True
+    return is_sorted
+
+def insertion_sort(lista, default_sort_criteria):
+    tamanio = size(lista)
+
+    for i in range(2, tamanio + 1):
+        j = i
+
+        while j > 1 and default_sort_criteria(get_element(lista, j), get_element(lista, j - 1)):
+            exchange(lista, j, j - 1)
+            j -= 1
+
+    return lista
+
+def selection_sort(my_list, sort_crit):
+    n = my_list["size"]
+    elements = my_list["elements"]
+    for i in range(n-1):
+        min_index = i
+        for j in range(i + 1,n):
+            if sort_crit(elements[j], elements[min_index]):
+                min_index = j
+
+        elements[i], elements[min_index] = elements[min_index], elements[i]
+
+    return my_list
+
+def shell_sort(my_list, default_sort_criteria):
+    size = my_list["size"]
+    gap = size // 2
+    while gap > 0:
+        for i in range(gap, size):
+            temp = my_list["elements"][i]
+            j = i
+            while j >= gap and default_sort_criteria(my_list["elements"][j - gap], temp) == False:
+                my_list["elements"][j] = my_list["elements"][j - gap]
+                j -= gap
+            my_list["elements"][j] = temp
+        gap //= 2
+    return my_list
+
+def merge_sort(my_list, sort_criteria):
+    elementos = my_list["elements"]
+    n = len(elementos)
+
+    def mezclar(inicio, medio, fin, auxiliar):
+        i = inicio
+        j = medio + 1
+        k = inicio
+
+        while i <= medio and j <= fin:
+            if sort_criteria(elementos[i], elementos[j]):
+                auxiliar[k] = elementos[i]
+                i += 1
+            else:
+                auxiliar[k] = elementos[j]
+                j += 1
+            k += 1
+
+        while i <= medio:
+            auxiliar[k] = elementos[i]
+            i += 1
+            k += 1
+
+        while j <= fin:
+            auxiliar[k] = elementos[j]
+            j += 1
+            k += 1
+
+        for pos in range(inicio, fin + 1):
+            elementos[pos] = auxiliar[pos]
+
+    def ordenar(inicio, fin, auxiliar):
+        if inicio < fin:
+            medio = (inicio + fin) // 2
+            ordenar(inicio, medio, auxiliar)
+            ordenar(medio + 1, fin, auxiliar)
+            mezclar(inicio, medio, fin, auxiliar)
+
+    if n > 1:
+        auxiliar = elementos.copy()
+        ordenar(0, n - 1, auxiliar)
+
+    return my_list
+
+def quick_sort(my_list, sort_criteria):
+    elementos = my_list["elements"]
+    n = len(elementos)
+
+    def dividir(lo, hi):
+        pivote = elementos[lo]
+        i = lo + 1
+        j = hi
+        terminado = False
+
+        while not terminado:
+            while i <= hi and sort_criteria(elementos[i], pivote):
+                i += 1
+            while j >= lo + 1 and sort_criteria(pivote, elementos[j]):
+                j -= 1
+            if i >= j:
+                terminado = True
+            else:
+                elementos[i], elementos[j] = elementos[j], elementos[i]
+                i += 1
+                j -= 1
+
+        elementos[lo], elementos[j] = elementos[j], elementos[lo]
+        return j
+
+    def ordenar(lo, hi):
+        if lo < hi:
+            posicion_pivote = dividir(lo, hi)
+            ordenar(lo, posicion_pivote - 1)
+            ordenar(posicion_pivote + 1, hi)
+
+    if n > 1:
+        random.shuffle(elementos)
+        ordenar(0, n - 1)
+
+    return my_list
--- DataStructures/List/single_linked_list.py
+++ DataStructures/List/single_linked_list.py
@@ -1,3 +1,4 @@
+import random
 def new_list():
     newlist = {
         "first": None,
@@ -212,3 +213,171 @@ def sub_list(my_list, pos, num_elements):
         sub_count += 1
 
     return newlist
+
+def default_sort_criteria(element_1, element_2):
+   is_sorted = False
+   if element_1 < element_2:
+      is_sorted = True
+   return is_sorted
+
+
+def selection_sort(my_list, sort_criteria):
+    actual = my_list["first"]
+
+    while actual is not None:
+        minimo = actual
+        siguiente = actual["next"]
+
+        while siguiente is not None:
+            if sort_criteria(siguiente["info"], minimo["info"]):
+                minimo = siguiente 
+
+            siguiente = siguiente["next"]
+        
+        temporal = actual["info"]
+        actual["info"] = minimo["info"]
+        minimo["info"] = temporal
+
+        actual = actual["next"]
+
+    return my_list 
+
+
+def insertion_sort(my_list, sort_criteria):
+    actual = my_list["first"]
+    ordenada = None 
+
+    while actual is not None: 
+        siguiente = actual["next"]
+
+        if ordenada is None or sort_criteria(actual["info"], ordenada["info"]):
+            actual["next"] = ordenada
+            ordenada = actual
+
+        else:
+            temp = ordenada
+
+            while temp["next"] is not None and sort_criteria(temp["next"]["info"], actual["info"]):
+                temp = temp["next"]
+
+            actual["next"] = temp["next"]
+            temp["next"] = actual
+
+        actual = siguiente
+    
+    my_list["first"] = ordenada 
+    return my_list
+
+def shell_sort(my_list, sort_criteria):
+    n = my_list["size"]
+    gap = n // 2
+
+    while gap > 0:
+        i = gap + 1
+        while i <= n:
+            temp = get_element(my_list, i)
+            j = i
+            while j > gap and sort_criteria(temp, get_element(my_list, j - gap)):
+                change_info(my_list, j, get_element(my_list, j - gap))
+                j -= gap
+
+            change_info(my_list, j, temp)
+            i += 1
+
+        gap //= 2
+
+    return my_list
+
+def merge(left, right, sort_criteria):
+    result = new_list()
+
+    i = 1
+    j = 1
+
+    while i <= left["size"] and j <= right["size"]:
+        elem_left = get_element(left, i)
+        elem_right = get_element(right, j)
+
+        if sort_criteria(elem_left, elem_right):
+            add_last(result, elem_left)
+            i += 1
+        else:
+            add_last(result, elem_right)
+            j += 1
+
+    while i <= left["size"]:
+        add_last(result, get_element(left, i))
+        i += 1
+
+    while j <= right["size"]:
+        add_last(result, get_element(right, j))
+        j += 1
+
+    return result
+
+
+def merge_sort(my_list, sort_criteria):
+    if my_list["size"] <= 1:
+        return my_list
+
+    mitad = my_list["size"] // 2
+
+    izq_half = sub_list(my_list, 1, mitad)
+    der_half = sub_list(my_list, mitad + 1, my_list["size"] - mitad)
+
+    izq_sorted = merge_sort(izq_half, sort_criteria)
+    der_sorted = merge_sort(der_half, sort_criteria)
+
+    return merge_(izq_sorted, der_sorted, sort_criteria)
+
+
+def quick_sort(my_list, sort_criteria):
+    n = my_list["size"]
+
+    def mezclar_posiciones():
+        for i in range(n - 1, 0, -1):
+            j = random.randint(0, i)
+            if i != j:
+                valor_i = get_element(my_list, i)
+                valor_j = get_element(my_list, j)
+                change_info(my_list, i, valor_j)
+                change_info(my_list, j, valor_i)
+
+    def particionar(lo, hi):
+        pivote = get_element(my_list, lo)
+        i = lo + 1
+        j = hi
+        terminado = False
+
+        while not terminado:
+            while i <= hi and sort_criteria(get_element(my_list, i), pivote):
+                i += 1
+            while j >= lo + 1 and sort_criteria(pivote, get_element(my_list, j)):
+                j -= 1
+            if i >= j:
+                terminado = True
+            else:
+                valor_i = get_element(my_list, i)
+                valor_j = get_element(my_list, j)
+                change_info(my_list, i, valor_j)
+                change_info(my_list, j, valor_i)
+                i += 1
+                j -= 1
+
+        valor_lo = get_element(my_list, lo)
+        valor_j = get_element(my_list, j)
+        change_info(my_list, lo, valor_j)
+        change_info(my_list, j, valor_lo)
+        return j
+
+    def ordenar(lo, hi):
+        if lo < hi:
+            posicion_pivote = particionar(lo, hi)
+            ordenar(lo, posicion_pivote - 1)
+            ordenar(posicion_pivote + 1, hi)
+
+    if n > 1:
+        mezclar_posiciones()
+        ordenar(0, n - 1)
+
+    return my_list
